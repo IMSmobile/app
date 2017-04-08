c97f9edc6dc5f2edf52715fd4ae8ce8b241686ec
Sandro Zbinden <sandrozbinden@gmail.com>
2017-04-08 17:14:23 +0200

    Add ChromeCanary

--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeCanary'],
     singleRun: false,
     customLaunchers: {
       Chrome_travis_ci: {
