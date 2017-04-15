9f0dd870a47ce845fa2df410d3e491da402896f8
Sandro Zbinden <sandrozbinden@gmail.com>
2017-04-16 01:45:24 +0200

    Disable form boarder which correct input

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { NavController, NavParams } from 'ionic-angular';
-import { LoadingController } from 'ionic-angular';
-
-
+import { NavController, NavParams, LoadingController, AlertController, ToastController } from 'ionic-angular';
 
 
 @Component({
@@ -14,7 +11,7 @@ export class LoginPage {
 
   loginForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formBuilder: FormBuilder, public loadingCtrl: LoadingController, public alertCtrl: AlertController, public toastCtrl: ToastController) {
     this.loginForm = this.formBuilder.group({
       url: ['', Validators.required],
       user: ['', Validators.required],
@@ -23,11 +20,30 @@ export class LoginPage {
   }
 
   login() {
-    let loader = this.loadingCtrl.create({
-      content: "Connecting to IMS Server",
-      duration: 3000
-    });
-    loader.present();
+    this.markAllAsTouched();
+    if (this.loginForm.invalid) {
+      let toast = this.toastCtrl.create({
+        message: 'Enter correct credentials',
+        duration: 3000,
+      });
+      toast.present();
+    } else {
+      let url = this.loginForm.controls['url'].value;
+      let alert = this.alertCtrl.create({
+        title: 'No Connection',
+        subTitle: 'Can\'t connect to server at' + url,
+        buttons: ['Dismiss']
+      });
+      alert.present();
+
+    }
+
+  }
+
+  markAllAsTouched() {
+    this.loginForm.controls['url'].markAsTouched();
+    this.loginForm.controls['user'].markAsTouched();
+    this.loginForm.controls['password'].markAsTouched();
   }
 
 }
