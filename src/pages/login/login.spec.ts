8fa32a5b806815193867383fa711984556b9fb62
Sandro Zbinden <sandrozbinden@gmail.com>
2017-04-17 20:23:59 +0200

    Create test for invalid and successfull login.

--- src/pages/login/login.spec.ts
+++ src/pages/login/login.spec.ts
@@ -6,11 +6,13 @@ import { Http, HttpModule, BaseRequestOptions } from '@angular/http';
 import { MockImsBackend } from '../../model/test/mock-ims-backend';
 import { AuthService } from '../../providers/auth-service';
 import { ImsService } from '../../providers/ims-service';
-import { ConfigMock, PlatformMock, NavParamsMock } from '../../model/test/mocks';
+import { ConfigMock, PlatformMock, NavParamsMock, ToastMock, AppMock, AlertMock } from '../../model/test/mocks';
+import { HomePage } from '../home/home';
+
 describe('Page: Login', () => {
 
   let fixture: ComponentFixture<LoginPage> = null;
-  let instance: any = null;
+  let page: LoginPage = null;
 
   beforeEach(async(() => {
 
@@ -19,7 +21,7 @@ describe('Page: Login', () => {
       declarations: [LoginPage],
 
       providers: [
-        App, DomController, Form, Keyboard, NavController, LoadingController, AlertController, ToastController, AuthService, ImsService, MockImsBackend, BaseRequestOptions,
+        App, DomController, Form, Keyboard, NavController, LoadingController, AlertController, AuthService, ImsService, MockImsBackend, BaseRequestOptions,
         {
           provide: Http,
           useFactory: (MockImsBackend, options) => {
@@ -27,14 +29,17 @@ describe('Page: Login', () => {
           },
           deps: [MockImsBackend, BaseRequestOptions]
         },
+        { provide: App, useClass: AppMock },
+        { provide: AlertController, useClass: AlertMock },
         { provide: Config, useClass: ConfigMock },
         { provide: Platform, useClass: PlatformMock },
         { provide: NavParams, useClass: NavParamsMock },
+        { provide: ToastController, useClass: ToastMock },
       ],
       imports: [HttpModule, FormsModule, IonicModule, ReactiveFormsModule]
     }).compileComponents().then(() => {
       fixture = TestBed.createComponent(LoginPage);
-      instance = fixture;
+      page = fixture.componentInstance;
       fixture.detectChanges();
     });
   }));
@@ -43,10 +48,21 @@ describe('Page: Login', () => {
     fixture.destroy();
   });
 
-  it('should create LoginPage', () => {
-    expect(fixture).toBeTruthy();
-    expect(instance).toBeTruthy();
-  });
+  it('Should popup toast message with warning message', inject([ToastController], (toastController: ToastController) => {
+    const spy = spyOn(toastController, 'create').and.callThrough();
+    page.login();
+    expect(toastController.create).toHaveBeenCalled();
+  }));
 
+  it('Load HomePage after successfull login', inject([NavController, MockImsBackend], (nav: NavController, mockImsBackend: MockImsBackend) => {
+    const spy = spyOn(nav, 'setRoot').and.callThrough();
+    let credential = mockImsBackend.credential;
+    page.loginForm.controls['server'].setValue(credential.server);
+    page.loginForm.controls['user'].setValue(credential.username);
+    page.loginForm.controls['password'].setValue(credential.password);
+    expect(page.loginForm.valid).toBeTruthy();
+    page.login();
+    expect(nav.setRoot).toHaveBeenCalledWith(HomePage);
+  }));
 
 });
