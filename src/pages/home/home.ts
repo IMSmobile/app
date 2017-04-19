794a400a964712c377a0501ec9722dc47d817f9e
Michael Leu <mleu@users.noreply.github.com>
2017-04-19 14:55:06 +0200

    indicate loading, sucess and failure of camera upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,10 +1,9 @@
 import { ImageEntry } from './../../model/imageEntry';
-import { Credential } from './../../model/credential';
 import { UploadService } from './../../providers/upload-service';
 import { AuthService } from './../../providers/auth-service';
 import { Component } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, Loading, AlertController, ToastController } from 'ionic-angular';
 import { Image } from '../../model/image';
 
 @Component({
@@ -13,9 +12,10 @@ import { Image } from '../../model/image';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public camera: Camera, public uploadService: UploadService, public authService: AuthService) { }
+  constructor(public navCtrl: NavController, public camera: Camera, public uploadService: UploadService, public authService: AuthService, public loadingCtrl: LoadingController, public alertCtrl: AlertController, public toastCtrl: ToastController) { }
   private imageSrc: string;
   private imageBlob: Blob;
+  private loading: Loading;
 
   public takePicture() {
 
@@ -35,11 +35,19 @@ export class HomePage {
   }
 
   public uploadPicture() {
+    this.showLoading();
     let imageEntry = new ImageEntry().set('IDFall', '20537').set('BILDNAME', 'Image from Camera');
     let image = new Image('SmartPhonePhoto.jpeg', this.imageBlob);
     this.uploadService.uploadImage(this.authService.currentCredential, 40, imageEntry, image).subscribe(
-      response => console.log(response),
-      err => console.warn(err));
+      response => {
+        this.hideLoading();
+        this.showToastMessage('Image successfully uploaded!');
+      },
+      err => {
+        this.hideLoading();
+        this.showAlert('Oops, something went wrong!');
+        console.warn(err);
+      });
   }
 
   private fixBinary(bin: string) {
@@ -52,4 +60,31 @@ export class HomePage {
     return buf;
   }
 
+  showAlert(message: string) {
+    let alert = this.alertCtrl.create({
+      title: 'Failed',
+      subTitle: message,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+  }
+
+  hideLoading() {
+    this.loading.dismiss();
+  }
+
+  showToastMessage(toastMessage: string) {
+    let toast = this.toastCtrl.create({
+      message: toastMessage,
+      duration: 5000,
+    });
+    toast.present();
+  }
 }
