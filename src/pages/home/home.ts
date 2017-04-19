55391e2630a0bf09970170d14e217f25f995eaae
Sandro Zbinden <sandrozbinden@gmail.com>
2017-04-19 19:40:59 +0200

    Correct image orientation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,7 +23,8 @@ export class HomePage {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation : true
     };
 
     this.camera.getPicture(options).then((imageData) => {
