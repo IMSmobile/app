3f9accac30d7ed3a68188c0aa5a00525456f0bf1
Michael Leu <mleu@users.noreply.github.com>
2017-05-01 20:35:30 +0200

    enable ionic package for iOS

--- scripts/run-ionic-cloud-builds-on-pr-or-master.sh
+++ scripts/run-ionic-cloud-builds-on-pr-or-master.sh
@@ -2,7 +2,7 @@
 set -ev
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] || [ "$TRAVIS_BRANCH" == "master" ]; then
-  npm run ionic:build && ionic package build android --noresources --profile dev
+  npm run ionic:build && ionic package build android --noresources --profile dev && ionic package build ios --noresources --profile dev
   rc=$?
 fi
 
