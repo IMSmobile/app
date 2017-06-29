04a0fc6151d7508125cee9d2c1fdd6930ab19235
Sandro Zbinden <sandrozbinden@gmail.com>
2017-06-29 18:38:58 +0200

    build: add automatic release creation with node script
    Automatic creation of changelog was not working because tags were not reachable from master as they got lost after the squash commit. The new described workflow allows release generation directly on master branch and shortens the release process.
    
    This closes #429

---
+++ scripts/release.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').exec;
+
+const standardVersion = require('standard-version');
+const packageJsonFile = path.resolve(__dirname, '../package.json');
+const configXmlFile = path.resolve(__dirname, '../config.xml');
+const loginTsFile = path.resolve(__dirname, '../src/pages/login/login.ts');
+var packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'utf8'));
+
+var versionBefore = packageJson['version'];
+
+standardVersion({ "skip": { "commit": true }, }).then(function succ() {
+  packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'utf8'));
+  var newVersion = packageJson['version'];
+  replaceInFile(configXmlFile, 'id="io.github.imsmobile.app" version="' + versionBefore + '"', 'id="io.github.imsmobile.app" version="' + newVersion + '"');
+  replaceInFile(loginTsFile, 'version: string = \'' + versionBefore + '\'', 'version: string = \'' + newVersion + '\'');
+  exec('git tag -d v' + newVersion);
+  exec('git tag ' + newVersion);
+});
+
+
+function replaceInFile(file, searchValue, replaceValue) {
+  var content = fs.readFileSync(file, 'utf8');
+  var contentReplaced = content.replace(searchValue, replaceValue);
+  fs.writeFileSync(file, contentReplaced, 'utf8');
+}
