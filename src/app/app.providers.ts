d24e10d09b2d011f3003f59ba136efd14bd0073a
Sandro Zbinden <sandrozbinden@gmail.com>
2017-07-04 21:04:22 +0200

    refactor: remove unused camera mock
    * style: remove unused imports

--- src/app/app.providers.ts
+++ src/app/app.providers.ts
@@ -5,25 +5,11 @@ import { BrowserContainerUploadService } from './../providers/browser-container-
 import { ContainerUploadService } from './../providers/container-upload-service';
 import { Http } from '@angular/http';
 import { TransferBlobMock } from './../mocks/providers/transfer-blob-mock';
-import { CameraMock } from './../mocks/providers/camera-mock';
 import { Transfer } from '@ionic-native/transfer';
 import { Provider } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { Camera } from '@ionic-native/camera';
 
 export class AppProviders {
-
-  public static getCameraProvider(): Provider {
-    return {
-      provide: Camera, useFactory: (platform: Platform) => {
-        if (this.deviceRunningCordova(platform)) {
-          return new Camera();
-        } else {
-          return new CameraMock();
-        }
-      }, deps: [Platform]
-    };
-  }
 
   public static getTransferProvider(): Provider {
     return {
