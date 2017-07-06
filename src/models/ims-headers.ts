07706ef2f68360e5092a7b6d0590ec4e3115ab5e
Michael Leu <mleu@users.noreply.github.com>
2017-07-06 21:10:20 +0200

    build: add lint rule  "restrict-plus-operands" and "prefer-template"
    using a template as a workaround when concatenating two different types in ImsBackendMock

--- src/models/ims-headers.ts
+++ src/models/ims-headers.ts
@@ -8,7 +8,7 @@ export class ImsHeaders extends Headers {
     super();
     this.append('Content-Type', 'application/json');
     this.append('Allow-Control-Allow-Origin', '*');
-    this.append('Authorization', 'Basic ' + btoa(credential.username + ':' + credential.password));
+    this.append('Authorization', 'Basic ' + btoa(`${credential.username}:${credential.password}`));
     if (token) {
       this.append('ims-rest-token', token.token);
     }
