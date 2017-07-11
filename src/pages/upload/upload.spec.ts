80ba6b2940e5be619086655c34e20b7dca136307
Michael Leu <mleu@users.noreply.github.com>
2017-07-11 21:00:58 +0200

    build: add lint rule "max-file-line-count"

--- src/pages/upload/upload.spec.ts
+++ src/pages/upload/upload.spec.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:max-file-line-count */
 import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BaseRequestOptions, Http, HttpModule, Response, ResponseOptions } from '@angular/http';
