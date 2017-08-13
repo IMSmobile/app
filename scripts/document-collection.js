8ea0dbac14557bbd39b4f068102644583e787d2f
Sandro Zbinden <sandrozbinden@gmail.com>
2017-08-13 10:52:16 +0200

    build: fix umlaut link and change export format to html

--- scripts/document-collection.js
+++ scripts/document-collection.js
@@ -6,36 +6,93 @@
  */
 const path = require('path');
 const spawn = require('child_process').spawn;
+const cheerio = require('cheerio')
 const execSync = require('child_process').execSync;
-
 const fs = require('fs-extra')
+const chalk = require('chalk')
+const figures = require('figures')
+
+const exportDirName = 'document-collection'
+const ignoredMDs = ['node_modules/**/*.md', 'platforms/**/*.md', 'plugins/**/*.md'];
 const rootDir = path.resolve(__dirname, '../.');
-const exportDir = path.resolve(__dirname, '../document-collection');
+const exportDir = path.resolve(__dirname, '../' + exportDirName);
 const glob = require('glob');
 
-
 fs.removeSync(exportDir);
+copyToExportDirectory('docs/**/*');
+copyToExportDirectory('e2e/**/*');
+copyToExportDirectory('src/**/*');
+copyToExportDirectory('scripts/**/*');
+copyToExportDirectory('*.md');
+copyToExportDirectory('*.json');
+copyToExportDirectory('*.xml');
+convertMDToHTML();
 
-glob("docs/**/*", {}, function (er, files) {
-  files.forEach(function (file) {
-    exportFile = exportDir + "/" + file;
-    if (!fs.lstatSync(file).isDirectory()) {
+function copyToExportDirectory(wildcard) {
+  glob(wildcard, {}, function (er, files) {
+    files.forEach(function (file) {
+      exportFile = exportDir + "/" + file;
+      if (!fs.lstatSync(file).isDirectory()) {
+        ensureDirectory(exportFile);
+        fs.createReadStream(file).pipe(fs.createWriteStream(exportFile));
+      }
+    });
+    console.info(chalk.green(figures.tick) + ' copied files with wildcard ' + wildcard + ' to export directory ' + exportDir);
+  });
+}
+
+function convertMDToHTML() {
+  glob("**/*.md", { 'ignore': ignoredMDs }, function (er, files) {
+    console.info(chalk.blue(figures.info) + ' converting all .md documents. This could take a while...');
+    files.forEach(function (file) {
+      exportFile = exportDir + "\\" + file.replace('.md', '.html');
       ensureDirectory(exportFile);
-      console.log('Create copy to file', exportFile)
-      fs.createReadStream(file).pipe(fs.createWriteStream(exportFile));
-    }
-  });
-})
+      execSync('grip ' + [file, '--quiet', '--export', exportFile].join(' '));
+      console.info(chalk.green(figures.tick) + ' successfull converted ' + file);
+    });
+    sanitizeHTMLLinks();
+  })
+}
 
-glob("**/*.md", { 'ignore': ['node_modules/**/*.md', 'platforms/**/*.md', 'plugins/**/*.md'] }, function (er, files) {
-  console.info("Converting all documents. This could take a while...")
-  files.forEach(function (file) {
-    console.info('Start converting', file);
-    exportFile = exportDir + "\\" + file;
-    ensureDirectory(exportFile);
-    execSync('grip ' + [file, '--export', exportFile].join(' '));
-  });
-})
+function sanitizeHTMLLinks() {
+  glob(exportDirName + '/**/*.html', {}, function (er, files) {
+    console.info(chalk.blue(figures.info) + ' sanitizing all .html documents. This could take a while...');
+    files.forEach(function (file) {
+      const $ = cheerio.load(fs.readFileSync(file));
+      $('a').each(function (index, link) {
+        if ($(link).attr('href')) {
+          replaceInternalMDLinkWithHTML($, link);
+          fixUmlautLinks($, link);
+        }
+      });
+      fs.writeFileSync(file, $.html());
+    });
+    console.info(chalk.green(figures.tick) + ' sanitized all html links');
+  })
+}
+
+
+function replaceInternalMDLinkWithHTML($, link) {
+  href = $(link).attr('href')
+  if (isInternalLink(href)) {
+    if (href.endsWith('.md')) {
+      $(link).attr('href', href.replace('.md', '.html'));
+    }
+  }
+}
+
+function fixUmlautLinks($, link) {
+  $(link).attr('href', $(link).attr('href').replace('%C3%BC', 'ü'));
+  $(link).attr('href', $(link).attr('href').replace('%C3%9c', 'Ü'));
+  $(link).attr('href', $(link).attr('href').replace('%C3%84', 'Ä'));
+  $(link).attr('href', $(link).attr('href').replace('%C3%A4', 'ä'));
+  $(link).attr('href', $(link).attr('href').replace('%C3%96', 'Ö'));
+  $(link).attr('href', $(link).attr('href').replace('%C3%B6', 'ö'));
+}
+
+function isInternalLink(href) {
+  return !href.startsWith('http');
+}
 
 function ensureDirectory(file) {
   if (!fs.existsSync(path.dirname(file))) {
